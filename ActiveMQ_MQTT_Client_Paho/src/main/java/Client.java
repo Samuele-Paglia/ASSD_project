257b2faad1d83570c2afde12ba3e51dc4e0ad780
Samu <sml.pgl@gmail.com>
2020-08-11 11:39:55 +0200

    Adding jars

--- ActiveMQ_MQTT_Client_Paho/src/main/java/Client.java
+++ ActiveMQ_MQTT_Client_Paho/src/main/java/Client.java
@@ -28,10 +28,10 @@ public class Client {
     	
 //    	long date = new Date().getTime();
     	
-//    	String topic = "ecs-topic";   	
-//    	String content = "{\"id\": \"ADNd345DfdDAF34AD347hHgNd345DfdDAFT3\", \"type\": \"device\", \"long\" : \"-73.85607\", \"lat\": \"41.848447\", \"timestamp\" : \"" + date + "\" }";
 //    	String topic = "ics-topic";
 //    	String content = "{\"uuidReceiver\": \"1b1ace19-805c-bce1-9d8b-79cbbd1dcbca\", \"uuidSender\": \"4edb83dd-1ecc-6473-cf6e-21fd82bade67\", \"rssi\": -79, \"txPower\": -59, \"timestamp\" : \"" + date + "\" }";
+//    	String topic = "ecs-topic";   	
+//    	String content = "{\"id\": \"ADNd345DfdDAF34AD347hHgNd345DfdDAFT3\", \"type\": \"device\", \"long\" : \"-73.85607\", \"lat\": \"41.848447\", \"timestamp\" : \"" + date + "\" }";
     	String topic = "tcn-topic";
     	String content = "fd8deb9d91a13e144ca5b0ce14e289532e040fe0bf922c6e3dadb1e4e2333c"
 				+ "78df535b90ac99bec8be3a8add45ce77897b1e7cb1906b5cff1097d3cb142f"
